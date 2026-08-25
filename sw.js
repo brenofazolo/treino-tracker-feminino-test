d32c2cbd303f7dc933dd0d518b6ea388cae04caa
BRENO FAZOLO <67841251+brenofazolo@users.noreply.github.com>
2026-08-25 19:59:28 -0300

    fix: refresh official brand-only theme cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='treino-feminino-test-v9';
+const CACHE='treino-feminino-test-v10';
 const ASSETS=['./index.html','./manifest.json','./config-tools.js','./history-tools.js','./workout-input-tools.js','./block-workout-tools.js','./female-enhancements.js','./lances-theme.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
