27ca68520a516d96ecf6907ee17b05644556127d
BRENO FAZOLO <67841251+brenofazolo@users.noreply.github.com>
2026-09-25 09:55:15 -0300

    Renova cache para botão verde de conclusão

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='treino-feminino-test-v13';
+const CACHE='treino-feminino-test-v14';
 const ASSETS=['./index.html','./manifest.json','./config-tools.js','./history-tools.js','./workout-input-tools.js','./block-workout-tools.js','./female-enhancements.js','./lances-theme.js','./appearance-tools.js','./lances-tech-logo.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
